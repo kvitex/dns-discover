dd0bae59d946d49cc34d67e7bf85e6b32ac05be7
Victor Kuryanov <kvitex@gmail.com>
2019-01-22 15:04:34 +0300

    fxed README and start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export FLASK_APP=dns-discover.py
 #export FLASK_ENV=development
-/usr/bin/env python3 -m flask run --host=127.0.0.1 --port=9053
+/usr/bin/env python3 -m flask run --host=$1 --port=$2
